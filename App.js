68d9c9045e3a66c228ea9fcd8e75d61a86e115d9
bros_ehiz <iyareehizogie@gmail.com>
2023-03-09 16:45:33 +0100

    todo

---
+++ App.js
@@ -0,0 +1,9 @@
+
+const addList = document.getElementById("entered").value;
+
+// let boy = "go home"
+function addItem(){
+    // e.preventDefault()
+    // main.innerText = entered-list
+    console.log(addList)
+}
